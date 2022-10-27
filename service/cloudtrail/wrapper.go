976e20a13cdc050eb50ac6da637d960785100bf3
zhuyicheng.881 <zhuyicheng.881@bytedance.com>
2022-10-27 16:07:17 +0800

    cloudtrail sdk

---
+++ service/cloudtrail/wrapper.go
@@ -0,0 +1,49 @@
+package cloudtrail
+
+import (
+	"encoding/json"
+	"net/url"
+	"strconv"
+)
+
+func (p *CloudTrail) LookupEvents(req *LookupEventsReq) (*LookupEventsResp, int, error) {
+	query := url.Values{}
+	resp := new(LookupEventsResp)
+
+	query.Set("X-Top-Account-Id", strconv.FormatInt(req.AccountID, 10))
+	reqBody, err := json.Marshal(req)
+	if err != nil {
+		return nil, 0, err
+	}
+	statusCode, err := p.commonHandlerJson("LookupEvents", query, reqBody, resp)
+	if err != nil {
+		return nil, statusCode, err
+	}
+	return resp, statusCode, nil
+}
+
+func (p *CloudTrail) commonHandlerQuery(api string, query url.Values, resp interface{}) (int, error) {
+	respBody, statusCode, err := p.Client.Query(api, query)
+	if err != nil {
+		return statusCode, err
+	}
+
+	if err = json.Unmarshal(respBody, resp); err != nil {
+		return statusCode, err
+	}
+	return statusCode, nil
+}
+
+func (p *CloudTrail) commonHandlerJson(api string, query url.Values, reqBody []byte, resp interface{}) (statusCode int, err error) {
+	var respBody []byte
+
+	respBody, statusCode, err = p.Client.Json(api, query, string(reqBody))
+	if err != nil {
+		return
+	}
+
+	if err = json.Unmarshal(respBody, resp); err != nil {
+		return
+	}
+	return
+}
